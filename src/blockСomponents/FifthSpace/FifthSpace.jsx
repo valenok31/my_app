0c52cb5d1fb412ae9db1c2a6f03cf35cc4a7234d
Andrey <priv1984@yandex.ru>
2022-01-30 12:14:40 +0700

    add typed text

--- src/blockСomponents/FifthSpace/FifthSpace.jsx
+++ src/blockСomponents/FifthSpace/FifthSpace.jsx
@@ -1,85 +1,27 @@
-// Render Prop
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+import style from './FifthSpace.module.css';
 
-let buttonLaps = "laps";
 const FifthSpace = (props) => {
 
-    const [time, setTime] = useState(0);
-    const [start, setStart] = useState(false);
-    const [laps, setLaps] = useState("00:00:00.00");
 
 
-    useEffect(() => {
-        let interval = null;
+ const [typedText, setTypedText] = useState('');
 
 
 
-        if (start) {
-            interval = setInterval(() => {
-                setTime(time => time + 10)
-            }, 10)
-        } else {
-            clearInterval(interval);
-        }
-
-        return () => clearInterval(interval)
-    }, [start])
-
-    let mSec = ("0" + (time / 10) % 1000).slice(-2);
-    let sec = ("0" + (Math.floor((time / 1000) % 60))).slice(-2);
-    let minut = ("0" + (Math.floor((time / 60000) % 60))).slice(-2);
-    let hour = ("0" + (Math.floor((time / 60000 / 60) % 60))).slice(-2);
-    let hmsmTime = hour + ":" + minut + ":" + sec + "." + mSec;
-
-
-    const timeLaps = () => {
-
-        if (time === 0) {
-
-            setLaps(hmsmTime);
-            buttonLaps = "laps";
-        }
-        if (time !== 0 && !start) {
-
-            setLaps(hmsmTime);
-            buttonLaps = "reset laps";
-        }
-        if (time !== 0 && start) {
-
-            setLaps(hmsmTime);
-            setTime(0);
-            buttonLaps = "laps";
-        }
-
-
-    }
-
-
-    return (
-        <div>
-            <h1>Stopwatch</h1>
-            <h1>
-                <span>{hmsmTime}</span>
-            </h1>
-            <div>
-                {(!start) ? <button onClick={() => setStart(true)}>Start</button> :
-                    <button onClick={() => {
-                        setStart(false);
-                        buttonLaps = "reset laps";
-                    }}>Stop</button>}
-                <button onClick={() => {
-                    setTime(0);
-                    setStart(false);
-                }}>Reset
-                </button>
-                <button onClick={() => timeLaps()}>
-                    {buttonLaps}
-                </button>
+    return (<>
+        <div className={style.item}>
+            <div className={style.example}>
+                <span>Here text for trening</span>
             </div>
-            <h1>
-                <span>{laps}</span>
-            </h1>
+            <div className={style.workingArea}>
+                <textarea cols='30' rows='10' value={typedText} onChange ={(e) => setTypedText(e.target.value)}></textarea>
+            </div>
+            <div className={style.result}>
+                <span>{typedText.length}</span>
+            </div>
         </div>
+       </>
     );
 }
 
--- src/blockСomponents/SeventhSpace/SeventhSpace.jsx
+++ src/blockСomponents/SeventhSpace/SeventhSpace.jsx
@@ -12,6 +12,8 @@ const SeventhSpace = (props) => {
     const [paused, setPaused] = useState('paused');
     const [reset, setReset] = useState(false);
 
+    const [typedText, setTypedText] = useState('');
+
     const createLaps = laps.map((element, index) => {
         return (<p key={index}>Laps {index + 1}: {element}
             <button onClick={() => remove(index)}>DELET</button>
@@ -39,48 +41,83 @@ const SeventhSpace = (props) => {
     let minut = ("0" + (Math.floor((time / 60000) % 60))).slice(-2);
     let hour = ("0" + (Math.floor((time / 60000 / 60) % 60))).slice(-2);
     let msmTime = minut + ":" + sec + "." + mSec;
-
+    let speed = Math.floor(typedText.length * 60 / (Math.floor(time / 1000)));
 
     return <div className={style.item}>
-        <div>
-            <h1>{msmTime}</h1>
-        </div>
-        <div>
-            {
-                !start ? (<button onClick={() => {
-                    setStart(true);
-                    setPaused('running')
-                }}>Start
-                </button>) : (<button onClick={() => {
+        <div className={style.item__clock}>
+            <div>
+                <h1>{msmTime}</h1>
+            </div>
+            <div>
+                {
+                    !start ? (<button onClick={() => {
+                        setStart(true);
+                        setPaused('running')
+                    }}>Start
+                    </button>) : (<button onClick={() => {
+                        setStart(false);
+                        setPaused('paused');
+                    }}>Stop
+                    </button>)
+                }
+
+
+                <button disabled={start} onClick={() => {
                     setStart(false);
+                    setTime(0);
+                    setLaps([]);
                     setPaused('paused');
-                }}>Stop
-                </button>)
-            }
+                    setReset(!reset)
+                    setTypedText('');
+                }}>Reset
+                </button>
 
-
-            <button disabled={start} onClick={() => {
-                setStart(false);
-                setTime(0);
-                setLaps([]);
-                setPaused('paused');
-                setReset(!reset)
-            }}>Reset
-            </button>
-
-            <button onClick={() => {
-                setStart(start);
-                setLaps([...laps, msmTime])
-            }}>Laps
-            </button>
+                <button onClick={() => {
+                    setStart(start);
+                    setLaps([...laps, msmTime])
+                }}>Laps
+                </button>
+            </div>
+            <div>
+                {createLaps}
+            </div>
+            <div className={style.arrowClock}>
+                <div className={reset ? style.arrowClock__arrow_min_1 : style.arrowClock__arrow_min_2}
+                     style={{animationPlayState: paused}}>
+                    <span className={style.arrow}></span>
+                </div>
+                <div className={reset ? style.arrowClock__arrow_sec_1 : style.arrowClock__arrow_sec_2}
+                     style={{animationPlayState: paused}}>
+                    <span className={style.arrow}></span>
+                </div>
+                <div className={reset ? style.arrowClock__arrow_milsec_1 : style.arrowClock__arrow_milsec_2}
+                     style={{animationPlayState: paused}}>
+                    <span className={style.arrow}></span>
+                </div>
+            </div>
         </div>
-        <div>
-            {createLaps}
+        <div className={style.itemere}>
+            <div className={style.example}>
+                <code>
+                    const [time, setTime] = useState(0);<br/>
+                    const [start, setStart] = useState(false);<br/>
+                    const [laps, setLaps] = useState([]);<br/>
+                    const [paused, setPaused] = useState('paused');<br/>
+                    const [reset, setReset] = useState(false);<br/>
+                </code>
+            </div>
+            <div className={style.workingArea}>
+                <textarea cols='40' rows='5' value={typedText} onChange={(e) => {
+                    setTypedText(e.target.value);
+                    setStart(true);
+                    setPaused('running');
+                }}></textarea>
+            </div>
+            <div className={style.result}>
+                <span>{time > 1000 ? speed : 'calculation...'}</span>
+                {/*<div><span>{typedText.length}</span></div>*/}
+            </div>
         </div>
-        <div className={reset ? style.arrowClock : style.arrowClock2} style={{animationPlayState: paused}}>
-            <span className={style.arrow}></span>
-        </div>
-
     </div>
 
 
