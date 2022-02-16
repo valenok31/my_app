66bd4f708eb92dc17b2053f65befa7158c44259c
Andrey <priv1984@yandex.ru>
2022-02-16 21:19:17 +0700

    weather forecast

--- src/blockСomponents/NinthSpace/NinthSpace.jsx
+++ src/blockСomponents/NinthSpace/NinthSpace.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import style from "./NinthSpace.module.css"
 import Sun from "./Sun";
 
+
 const NinthSpace = (props) => {
     let temWos = props.tempe;
     let sunDay = <img
-        src='https://cdn-icons.flaticon.com/png/512/3073/premium/3073665.png?token=exp=1644940669~hmac=b2a4ff18e8416a4e7dcd6f30ef5f8046'/>
+        src="https://cdn-icons.flaticon.com/png/512/3073/premium/3073665.png?token=exp=1644940669~hmac=b2a4ff18e8416a4e7dcd6f30ef5f8046"/>
     let nightDay = <img
         src='https://cdn-icons-png.flaticon.com/512/740/740878.png'/>
 
--- src/blockСomponents/NinthSpace/Sun.js
+++ src/blockСomponents/NinthSpace/Sun.js
@@ -4,13 +4,12 @@ import style from './NinthSpace.module.css'
 const Sun = (props) => {
 
     let temWos = props.tempe;
-    let sunDay = <img
-        src='https://cdn-icons.flaticon.com/png/512/3073/premium/3073665.png?token=exp=1644940669~hmac=b2a4ff18e8416a4e7dcd6f30ef5f8046'/>
-    let nightDay = <img
-        src='https://cdn-icons-png.flaticon.com/512/740/740878.png'/>
+    let sunDay = <img src="https://cdn-icons.flaticon.com/png/512/3073/premium/3073665.png?token=exp=1644940669~hmac=b2a4ff18e8416a4e7dcd6f30ef5f8046"/>
+    let nightDay = <img src="https://cdn-icons-png.flaticon.com/512/740/740878.png"/>
 
     return <div>
-        <div className={style.sun}>{(props.dateNow > temWos.sunset || props.dateNow < temWos.sunrise) ? nightDay : sunDay}</div>
+        <div
+            className={style.sun}>{(props.dateNow > temWos.sunset || props.dateNow < temWos.sunrise) ? nightDay : sunDay}</div>
         <div>{props.dateNow}</div>
 
     </div>
