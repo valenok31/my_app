6555084061f64623b73ed96642d8ee14d8147474
Andrey <priv1984@yandex.ru>
2022-02-16 20:33:07 +0700

    learn.javascript.ru

--- src/blockСomponents/FourthSpace/FourthSpace.jsx
+++ src/blockСomponents/FourthSpace/FourthSpace.jsx
@@ -1,41 +1,19 @@
 import React from "react";
 import style from "./FourthSpace.module.css"
-import axios from "axios";
-import Loader from "../Loader/Loader";
 
 const FourthSpace = (props) => {
 
-    let setUserId = (e) => {
-        props.togglesIsFetching(true);
-        let buttonValue = e.target.name;
-        props.setUserId(buttonValue);
+    const array = [1,3,5,30,10];
+    const result = array.lastIndexOf(3)
+    console.log(result);
 
-        axios.get(`https://jsonplaceholder.typicode.com/photos/` + buttonValue)
-            .then(resp => {
-                props.togglesIsFetching(false);
-                props.setUsersProfile(resp.data.title);
-            })
-            .catch(resp => {
-                props.togglesIsFetching(false);
-                props.setUsersProfile('ERROR!!');
-            })
-    }
 
-    let arr = [];
-    for (let i = 1; i < 21; i++) {
-        arr.push(i);
-    }
-    let buttt = arr.map((e) => <button onClick={setUserId} name={e}>{e}</button>)
     return (
         <div>
-            {props.isFetching ? <Loader/> : null}
             <div className={style.item}>
-                <div>{props.profile}</div>
-                <div>{props.userId}</div>
+                Hi!
             </div>
-            <div className={style.item}>
-                {buttt}
-            </div>
+
 
         </div>
     )
--- src/blockСomponents/FourthSpace/FourthSpaceCont.js
+++ src/blockСomponents/FourthSpace/FourthSpaceCont.js
@@ -1,52 +1,17 @@
 import React from "react";
 import {connect} from "react-redux";
-import {setUserId, setUsersProfile} from "../../redux/fourthSpace_reducer";
-import axios from "axios";
 import FourthSpace from "./FourthSpace";
-import {togglesIsFetching} from "../../redux/generalSetting_reducer";
 
 class FourthSpaceAPICont extends React.Component {
 
-    componentDidMount() {
-debugger
-
-        axios.get(`https://jsonplaceholder.typicode.com/photos/` + this.props.userId)
-            .then(resp => {
-                debugger
-                this.props.setUsersProfile(resp.data.thumbnailUrl);
-            })
-            .catch(resp => {
-                debugger
-                this.props.setUsersProfile(resp);
-            })
-
-
-/*        axios.get(`https://jsonplaceholder.typicode.com/photos/` + this.props.userId)
-            .then(resp => {
-                debugger
-                this.props.setUsersProfile(resp.data.title);
-            })
-            .catch(resp => {
-                this.props.setUsersProfile('ERROR!!');
-            })*/
-    }
-
     render() {
 
-        return <FourthSpace
-            isFetching={this.props.isFetching}
-            togglesIsFetching={this.props.togglesIsFetching}
-            setUsersProfile={this.props.setUsersProfile}
-            profile={this.props.profile}
-            setUserId={this.props.setUserId}
-            userId={this.props.userId}/>
+        return <FourthSpace />
     }
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.fourthSpace_reducer.profile,
-    userId: state.fourthSpace_reducer.userId,
-    isFetching: state.generalSetting_reducer.isFetching
+
 })
 
-export default connect(mapStateToProps, {setUsersProfile, setUserId, togglesIsFetching})(FourthSpaceAPICont)
+export default connect(mapStateToProps, {})(FourthSpaceAPICont)
